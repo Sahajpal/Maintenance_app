cd2a5eb9684c166dfc9a8e2460abfd01001b0e66
Sahajpal <sahajpal80@gmail.com>
2021-05-09 12:37:21 +0530

    Implemented Splash Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:maintenance_app/screens/home_page.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () => Navigator.push(context, MaterialPageRoute(builder: (context) => Home())));
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/jrew_logo.png',
+            height: 300,
+          ),
+          SpinKitCircle(
+            color: Colors.amber,
+            size: 100,
+          ),
+        ],
+      ),
+    );
+  }
+}
